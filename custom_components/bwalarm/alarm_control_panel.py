ec369b7db9f028b7a055ea1d06c9edb80d74f917
Akasma74 <akasma74@users.noreply.github.com>
2021-01-23 15:20:05 +0000

    only allow non-malicious paths

--- custom_components/bwalarm/alarm_control_panel.py
+++ custom_components/bwalarm/alarm_control_panel.py
@@ -477,16 +477,16 @@ class BwResources(HomeAssistantView):
 
     async def get(self, request, path):
         """Retrieve file."""
-        path = sanitize_path(path)
-        override_path = "{}/{}".format(self.override_folder, path)
-        default_path = "{}/{}".format(self.default_folder, path)
+        safe_path = sanitize_path(path)
+        if path != safe_path:
+            raise web.HTTPBadRequest
+        override_path = "{}/{}".format(self.override_folder, safe_path)
+        default_path = "{}/{}".format(self.default_folder, safe_path)
 
         if os.path.exists(override_path):
             return web.FileResponse(override_path)
         elif os.path.exists(default_path):
             return web.FileResponse(default_path)
-#        else:
-#            return None
 
 class BWAlarm(AlarmControlPanelEntity):
 
